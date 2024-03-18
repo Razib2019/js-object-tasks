42311f8629ea393bbc2313dc43cda6b49f657fad
Razib2019 <hafizurrahmanrazib@gmail.com>
2024-03-18 09:54:15 +0600

    questions initialize

---
+++ task_3.js
@@ -0,0 +1,13 @@
+/***
+Question: Display the "physics" marks as output.
+
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
+    }
+};
+ */
---
+++ task_4.js
@@ -0,0 +1,14 @@
+/***
+Question: Count the "number of properties".
+
+Input:
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+
+Output:
+4
+ */
